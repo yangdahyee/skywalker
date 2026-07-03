265ac9ee882f7879a34b2a184905e01dfc9988c6
양다혜 <yangdahye.e@daum.net>
2026-07-03 22:19:45 +0900

    🐛 fix: 자식 컨테이너 높이 중첩 오류 수정 및 모바일 화이트아웃 해결
    - App 최외곽 부모 레이어와 자식 레이어 간의 'min-h-screen' 중첩으로 인한 높이 0px 버그 수정
    - 자식 Outlet 컨테이너를 기존에 정상 작동하던 'h-full' 구조로 원상복구

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,7 @@ function App() {
     // h-screen 대신 min-h-screen로 함. 모바일에서 컨텐츠가 길어지면 아래로 시원하게 스크롤이 열리도록 함
     // 데스크탑 환경(md:)에서만 원래 의도대로 h-screen(화면 꽉 채움)과 overflow-hidden이 작동하도록 제어.
     <div className={`relative w-full min-h-screen md:h-screen text-stone-100 overflow-x-hidden overflow-y-auto md:overflow-hidden select-none ${shouldApplyCursorNone ? "cursor-none" : ""}`}>
-      {/* 자식 컴포넌트들이 부모의 높이를 유연하게 따라가도록 h-full 대신 min-h-screen md:h-full로 유연성 줌. */}
-      <div className="relative z-10 w-full min-h-screen md:h-full">
+      <div className="relative z-10 w-full h-full">
         <Outlet />
       </div>
 
